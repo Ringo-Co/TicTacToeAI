0df064e3ff6c282f6e2d119875bc422010abdc9f
Aiden <aiden.dielschneider@gmail.com>
2025-01-28 23:34:19 -0600

    Basic functionality

--- main.py
+++ main.py
@@ -5,16 +5,74 @@
 ###############################################################################
 # Imports and Global Variables ------------------------------------------------
 import random
-import tabulate
+from tabulate import tabulate
 board = [['1', '2', '3'], 
          ['4', '5', '6'], 
          ['7', '8', '9']]
+playing = True
+player_letter = 'X'
+ai_letter = 'O'
 
-def main():
+
+def print_board():
     print(tabulate(board,
                    tablefmt="rounded_grid",
                    stralign='center',
                    rowalign='center'))
+    
+
+def reset_board():
+    global board
+    board = [['1', '2', '3'], 
+             ['4', '5', '6'], 
+             ['7', '8', '9']]
+    
+
+def check_win(letter):
+    ''' Checks if "letter" has won.'''
+    for row in board:  # Horizontal
+        if all(cell == letter for cell in row):
+            return True
+    for col in range(3):  # Vertical
+        if all(board[row][col] == letter for row in range(3)):
+            return True
+        else:
+            continue
+    for col in range(3):  # Diagonal
+        if all(board[i][i] == letter for i in range(3)):
+            return True
+        if all(board[i][2-i] == letter for i in range(3)):
+            return True
+        return False
+    
+
+def player_turn():
+    while True:
+        print_board()
+        print(f"You are playing as {player_letter}.")
+        try:
+            choice = int(input("Choose a space: ").strip())
+            if choice < 1 or choice > 9:
+                print("ERROR: Invalid input.")
+                continue
+            row, col = divmod(choice - 1, 3)
+            if board[row][col]not in [player_letter, ai_letter]:
+                board[row][col] = player_letter
+            else:
+                print("Spot already taken!")
+                continue
+        except ValueError:
+            print("ERROR: Input was not a number.")
+
+
+def ai_turn():
+    pass
+
+
+def main():
+    while playing:
+        player_turn()
+        ai_turn()
 
 
 # Main ------------------------------------------------------------------------
